feb0d1d6711d1959c3f195e9708d56d48cb019dd
Roshan S. Bisne <134048911+rsbisne@users.noreply.github.com>
2026-01-16 18:46:19 +0530

    SQL Query for Grouping Sales Dataset

---
+++ Total_Sales_Data.sql
@@ -0,0 +1,9 @@
+SELECT * FROM `Sales.Sales_01`
+UNION ALL
+SELECT * FROM `Sales.Sales_02`
+UNION ALL
+SELECT * FROM `Sales.Sales_03`
+UNION ALL
+SELECT * FROM `Sales.Sales_04`
+UNION ALL
+SELECT * FROM `Sales.Sales_05`;
